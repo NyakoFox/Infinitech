542f74cfd9a6c8f0c409da92a8c89bba1e26e939
NyakoFox <nyakowofox@gmail.com>
2021-07-12 16:04:31 -0300

    change defaults

---
+++ src/main/java/gay/nyako/infinitech/InfinitechMod.java
@@ -0,0 +1,10 @@
+package gay.nyako.infinitech;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InfinitechMod implements ModInitializer {
+	@Override
+	public void onInitialize() { // modid is "infinitech"
+		System.out.println("hi from infinitech!!");
+	}
+}
