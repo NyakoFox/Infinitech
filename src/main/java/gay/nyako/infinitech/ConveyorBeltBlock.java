78749c9ce9089e1bf3d13e1634e796a9ca4d7df6
NyakoFox <nyakowofox@gmail.com>
2021-07-13 15:20:10 -0300

    attempt directional conveyor block

--- src/main/java/gay/nyako/infinitech/ConveyorBeltBlock.java
+++ src/main/java/gay/nyako/infinitech/ConveyorBeltBlock.java
@@ -2,29 +2,52 @@ package gay.nyako.infinitech;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 import java.util.stream.Stream;
 
-public class ConveyorBeltBlock extends Block {
+public class ConveyorBeltBlock extends HorizontalFacingBlock {
 
     public ConveyorBeltBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(Properties.HORIZONTAL_FACING);
     }
 
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        return Stream.of(
-                Block.createCuboidShape(1, 0, 0, 15, 2, 16),
-                Block.createCuboidShape(0, 0, 0, 1, 4, 16),
-                Block.createCuboidShape(15, 0, 0, 16, 4, 16)
-        ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+        Direction dir = state.get(Properties.HORIZONTAL_FACING);
+        return switch (dir) {
+            default -> Stream.of(
+                    Block.createCuboidShape(1, 0, 0, 15, 2, 16),
+                    Block.createCuboidShape(0, 0, 0, 1, 4, 16),
+                    Block.createCuboidShape(15, 0, 0, 16, 4, 16)
+            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+            case EAST, WEST -> Stream.of(
+                    Block.createCuboidShape(0, 0, 1, 16, 2, 15),
+                    Block.createCuboidShape(0, 0, 15, 16, 4, 16),
+                    Block.createCuboidShape(0, 0, 0, 16, 4, 1)
+            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+        };
     }
 
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState().with(Properties.HORIZONTAL_FACING, context.getPlayerFacing());
+    }
 }
